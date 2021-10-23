3fbd4a6228588c925ff91660f5b6f6605c142155
Arturas-st <arrrtas@gmail.com>
2021-10-23 17:36:04 +0200

    added deluppgift 2: konfigurera upp uart för sändning (tx)

--- main.c
+++ main.c
@@ -1,7 +1,17 @@
 #include <avr/io.h>
-
+#include <util/delay.h>
 #include "serial.h"
 
 void main (void) {
-    ledBlink();
+    //ledBlink();
+
+    uart_init();
+
+    while(1){
+        //uart_putchar('a');
+        //char *string = "hallo där\n";
+        //uart_putstr(string);
+        uart_putstr("hej där");
+        _delay_ms(500);
+    }
 }
--- serial.c
+++ serial.c
@@ -7,4 +7,54 @@
 #include <util/delay.h>
 #include "serial.h"
 
+#define BAUD 38400
+#define FOSC 16000000
+#define UBBR (FOSC/(16*BAUD) - 1)
 
+//uint16_t ubbr = FOSC/(16*BAUD) - 1;
+
+void uart_init(void){
+    //Set baud rate
+    UBRR0H = (UBBR >> 8);
+    UBRR0L = UBBR;
+
+    //Enable receiver and transmitter
+    UCSR0B |= (1 << RXEN0) | (1 << TXEN0);
+
+    //Set frame format: 8data, 2stop bit 
+    UCSR0C = (1<<USBS0)|(3<<UCSZ00);
+
+    //Set frame format: 8data, 1stop bit (8N1)
+    //UCSR0C = (1<<UCSZ01)|(1<<UCSZ00);
+}
+
+
+void uart_putchar(char chr){
+    // Wait for empty transmit buffer
+    while(!(UCSR0A & (1 << UDRE0))){ };
+    //Put data into buffer, sends the data
+    UDR0 = chr;
+
+    if(chr == '\n'){
+        uart_putchar('\r');
+    }
+
+}
+
+void uart_putstr(const char *str){
+    /*for(int i = 0; str[i] != '\0'; i++){
+        uart_putchar(str[i]);
+    }*/
+
+    char chr = *str;
+    uart_putchar(chr);
+    while (chr != '\0'){
+        str++;
+        chr = *str;
+        uart_putchar(chr);
+    }
+
+
+
+
+}
