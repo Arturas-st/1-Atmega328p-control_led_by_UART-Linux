c252bd58cbfeef36493979f0e1dbf4a6107b2c96
Arturas-st <arrrtas@gmail.com>
2021-10-24 15:21:06 +0200

    added deluppgift 3: konfigurera upp uart för mottagning (rx)

--- main.c
+++ main.c
@@ -1,17 +1,18 @@
 #include <avr/io.h>
 #include <util/delay.h>
+#include <stdlib.h>
 #include "serial.h"
+#include "blink.h"
 
-void main (void) {
+int main (void) {
     //ledBlink();
-
     uart_init();
-
     while(1){
         //uart_putchar('a');
         //char *string = "hallo där\n";
         //uart_putstr(string);
-        uart_putstr("hej där");
-        _delay_ms(500);
+        //uart_putstr("hej där\n");
+        uart_echo();
+        //_delay_ms(500);
     }
 }
--- serial.c
+++ serial.c
@@ -1,7 +1,5 @@
 #include <avr/io.h>
 #include <util/delay.h>
-
-#include "serial.h"
 #include <stdio.h>
 #include <avr/io.h>
 #include <util/delay.h>
@@ -17,15 +15,12 @@ void uart_init(void){
     //Set baud rate
     UBRR0H = (UBBR >> 8);
     UBRR0L = UBBR;
-
-    //Enable receiver and transmitter
-    UCSR0B |= (1 << RXEN0) | (1 << TXEN0);
-
-    //Set frame format: 8data, 2stop bit 
-    UCSR0C = (1<<USBS0)|(3<<UCSZ00);
-
+    //Enable transmitter
+    UCSR0B |= (1 << TXEN0);
+    //Enable receiver
+    UCSR0B |= (1 << RXEN0);
     //Set frame format: 8data, 1stop bit (8N1)
-    //UCSR0C = (1<<UCSZ01)|(1<<UCSZ00);
+    UCSR0C = (1<<UCSZ01)|(1<<UCSZ00);
 }
 
 
@@ -45,16 +40,30 @@ void uart_putstr(const char *str){
     /*for(int i = 0; str[i] != '\0'; i++){
         uart_putchar(str[i]);
     }*/
-
-    char chr = *str;
-    uart_putchar(chr);
+    char chr = *str; // get character
+    uart_putchar(chr); //send character
     while (chr != '\0'){
-        str++;
-        chr = *str;
-        uart_putchar(chr);
+        str++;             // increment the pointer
+        chr = *str;        // get next character
+        uart_putchar(chr); // send next character
     }
+}
 
+char uart_getchar(void){
+    // Wait for data to be received
+    while(!(UCSR0A & (1 << RXC0))){ };
+    // Get and return received data from buffer
+    return UDR0;
 
+}
 
+void uart_echo(void){
+    // Prints out what we enter in our terminal
+    uart_putchar(uart_getchar());
+    
 
+    //char chr = uart_getchar();
+    //uart_putchar(chr);
+        
+       
 }
